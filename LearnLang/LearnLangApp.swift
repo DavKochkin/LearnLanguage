1a27f8214b8294f8cd3ca436beeaa99595cd0e57
David <kochkin.david@gmail.com>
2023-03-30 12:11:50 +0300

    Initial Commit

---
+++ LearnLang/LearnLangApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearnLangApp.swift
+//  LearnLang
+//
+//  Created by David Kochkin on 30.03.2023.
+//
+
+import SwiftUI
+
+@main
+struct LearnLangApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
